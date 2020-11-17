47d9a92387b7ada884705dcb3c17ce4704f5e558
Ariel Jakubowski <49914510+JakubowskiA@users.noreply.github.com>
2020-11-16 20:08:58 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+ <BrowserRouter>
+  <Provider store={store}>
+   <App />
+  </Provider>
+ </BrowserRouter>,
+ document.getElementById('root')
+);
